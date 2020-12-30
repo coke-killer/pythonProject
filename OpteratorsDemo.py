0fc72e206ef76661541dd684d95cb17abb49eca3
yudongyue <yudongyue@daqo.com>
2020-12-30 15:01:23 +0800

    添加dictionary demo,类似map

---
+++ OpteratorsDemo.py
@@ -0,0 +1,49 @@
+"""
+python 算数运算符
+"""
+a = 10
+b = 101
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)  # 10的101次方
+print(a // b)  # 9//2=4 -9//2=-5
+"""
+比较运算符
+"""
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a <= b)
+print(a < b)
+print(a > b)
+"""
+复制运算符
+"""
+a += b
+print(a)
+print(b)
+print(a)
+a *= b
+print(a)
+print(b)
+"""位运算符"""
+a = 60
+b = 13
+print(a & b)
+print(a | b)
+print(a ^ b)
+print(~a)
+"""
+逻辑运算符 x与y
+and x为false  返回x
+or x为true 返回x
+not 返回true false
+"""
+"""
+成员运算符，成员是指字符串、列表或者元组
+in x在y序列中返回true
+not in 
+"""
