29ee78b41a3f935a278dca48e5a11e4a4cf62d99
yudongyue <yudongyue@daqo.com>
2020-12-30 13:49:26 +0800

    变量赋值

---
+++ dataType.py
@@ -0,0 +1,13 @@
+# python使用前不需要声明，变量使用前必须赋值才能被创建
+"""
+变量就是变量，没有类型，类型是指变量所指内存种对象的类型
+"""
+counter = 100  # 整形变量
+miles = 1000.0  # 浮点型变量
+name = "hello world"  # 字符串
+print(counter)
+print(miles)
+print(name)
+# 多个变量赋值
+a = b = c = 1
+a1, b1, c1, = 1, 2.0, "hello world",
